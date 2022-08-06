8a1c7d84ab6af86ea1f01c316bf482f9569251a6
shubham8004 <110707199+shubham8004@users.noreply.github.com>
2022-08-06 20:59:45 +0530

    Update MantissaFromDecimal.py

--- Function Conversion Problem Statement/Number functions/MantissaFromDecimal.py
+++ Function Conversion Problem Statement/Number functions/MantissaFromDecimal.py
@@ -1,4 +1,6 @@
 #using ADF toFloat function
 def float_wrapper(inp):
-    otp = toFloat(inp) 
-    return otp
+    # Assuming the number is a decimal point number , we will first convert it to string and then split by '.'  (decimal) and take the last portion
+    inp_string = toString(inp)
+    manitssa_part = split(inp_string, '.')[1]
+    return manitssa_part
