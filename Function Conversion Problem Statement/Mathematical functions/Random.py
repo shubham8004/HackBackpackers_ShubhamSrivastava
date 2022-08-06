42fc33f514f9bfe838e33d7a0ff52cf526cb0e98
shubham8004 <110707199+shubham8004@users.noreply.github.com>
2022-08-06 20:47:29 +0530

    Update Random.py

--- Function Conversion Problem Statement/Mathematical functions/Random.py
+++ Function Conversion Problem Statement/Mathematical functions/Random.py
@@ -1,4 +1,4 @@
 # Using ADF random function 
-def random_wrapper(inp):
-    otp = random(inp)  
+def random_wrapper(seed = 0):  # Setting the seed as 0 as Data Stage's random function returns value from 0 to pow(2,32)-1. 
+    otp = random(seed)  
     return otp
