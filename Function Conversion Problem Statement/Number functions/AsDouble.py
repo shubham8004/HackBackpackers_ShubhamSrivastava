cb6cd3d1695ea7ed230c651e0cc174baa9cf1813
shubham8004 <110707199+shubham8004@users.noreply.github.com>
2022-08-06 20:55:15 +0530

    Update AsDouble.py

--- Function Conversion Problem Statement/Number functions/AsDouble.py
+++ Function Conversion Problem Statement/Number functions/AsDouble.py
@@ -1,4 +1,5 @@
 #using ADF toDouble function
 def double_wrapper(inp):
-    otp = toDouble(inp) 
+    updated_inp = inp / 4.32  # Seeing the Data Stage function example I am dividng the number by 4.32
+    otp = toDouble(updated_inp) 
     return otp
